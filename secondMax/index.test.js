112d1a0d99c1a190c394df5737ca5a7bdf0b7042
OMAR RAMADAN <ramomar1992@outlook.com>
2021-10-14 11:46:48 +0300

    formates the files

--- secondMax/index.test.js
+++ secondMax/index.test.js
@@ -17,30 +17,33 @@ secondMax([-1, -5, 2])== -1
  * @return {number} number
  */
 
-const secondMax = function(array) {
+const secondMax = function (array) {
   let firstMax = array[0];
   let secondMax = array[0];
-  for(let i = 1; i < array.length ; i++){
-    if (array[i] > firstMax){
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > firstMax) {
       secondMax = firstMax;
       firstMax = array[i];
-    }else if((array[i] > secondMax) || (firstMax === secondMax && array[i]!== secondMax)){
-      secondMax = array[i]
-    }else{
+    } else if (
+      array[i] > secondMax ||
+      (firstMax === secondMax && array[i] !== secondMax)
+    ) {
+      secondMax = array[i];
+    } else {
       continue;
     }
   }
-  return secondMax !== firstMax? secondMax: null;
+  return secondMax !== firstMax ? secondMax : null;
 };
 
-describe('secondMax function', () => {
-  it('should return the second max number in each array of numbers', () => {
-    expect(secondMax([1,5, 2])).toEqual(2);
+describe("secondMax function", () => {
+  it("should return the second max number in each array of numbers", () => {
+    expect(secondMax([1, 5, 2])).toEqual(2);
     expect(secondMax([-1, -5, 2])).toEqual(-1);
     expect(secondMax([-1, -5, 2, 5])).toEqual(2);
-    expect(secondMax([15, 20 -3 , 133 , 50, 32])).toEqual(50);
-    expect(secondMax([100 , 133 , 50, 32])).toEqual(100);
-    expect(secondMax([133 , 50, 32])).toEqual(50);
-    expect(secondMax([100 , 100, 100])).toEqual(null);
+    expect(secondMax([15, 20 - 3, 133, 50, 32])).toEqual(50);
+    expect(secondMax([100, 133, 50, 32])).toEqual(100);
+    expect(secondMax([133, 50, 32])).toEqual(50);
+    expect(secondMax([100, 100, 100])).toEqual(null);
   });
 });
