2fe6759b280260f231a639af7b7f9781146b2fdf
OMAR RAMADAN <ramomar1992@outlook.com>
2021-10-14 11:42:32 +0300

    sec max function

--- secondMax/index.test.js
+++ secondMax/index.test.js
@@ -10,6 +10,37 @@ secondMax([1,5, 2])== 2
 secondMax([-1, -5, 2])== -1
 */
 
+/**
+ * @description Function takes in an array of numbers
+ * and returns The second maximum number in the array
+ * @param {array} array Array of numbers
+ * @return {number} number
+ */
+
 const secondMax = function(array) {
-  // TODO
+  let firstMax = array[0];
+  let secondMax = array[0];
+  for(let i = 1; i < array.length ; i++){
+    if (array[i] > firstMax){
+      secondMax = firstMax;
+      firstMax = array[i];
+    }else if((array[i] > secondMax) || (firstMax === secondMax && array[i]!== secondMax)){
+      secondMax = array[i]
+    }else{
+      continue;
+    }
+  }
+  return secondMax !== firstMax? secondMax: null;
 };
+
+describe('secondMax function', () => {
+  it('should return the second max number in each array of numbers', () => {
+    expect(secondMax([1,5, 2])).toEqual(2);
+    expect(secondMax([-1, -5, 2])).toEqual(-1);
+    expect(secondMax([-1, -5, 2, 5])).toEqual(2);
+    expect(secondMax([15, 20 -3 , 133 , 50, 32])).toEqual(50);
+    expect(secondMax([100 , 133 , 50, 32])).toEqual(100);
+    expect(secondMax([133 , 50, 32])).toEqual(50);
+    expect(secondMax([100 , 100, 100])).toEqual(null);
+  });
+});
