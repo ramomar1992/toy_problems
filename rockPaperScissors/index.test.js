3396bcf7b9cb260c990dd37939820704fa1cf7c9
OMAR RAMADAN <ramomar1992@outlook.com>
2021-10-17 14:12:31 +0300

    solves rock paper scissors

--- rockPaperScissors/index.test.js
+++ rockPaperScissors/index.test.js
@@ -16,6 +16,40 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
-const rockPaperScissors = function(num) {
 
+/**
+ *
+ * @param {number} num the number of trails the player does
+ * @param {Array} variation strings represents all possible results
+ * for each single trail
+ * @returns {Array} array of arrays to all possible outcome for all trails
+ */
+
+const rockPaperScissors = function(
+    num,
+    vriation = ['rock', 'paper', 'scissor'],
+) {
+  if (num == 0) return [];
+  const result = [];
+  const res = [...rockPaperScissors(num - 1, vriation)];
+  for (let i = 0; i < vriation.length; i++) {
+    if (res.length > 0) {
+      for (let j = 0; j < res.length; j++) {
+        result.push([vriation[i], ...res[j]]);
+      }
+    } else {
+      result.push([vriation[i]]);
+    }
+  }
+  return result;
 };
+
+
+describe('rockPaperScissors function', () => {
+  it('sohuld return the number of all trails', () => {
+    expect(rockPaperScissors(3).length).toStrictEqual(27);
+    expect(rockPaperScissors(0).length).toStrictEqual(0);
+    expect(rockPaperScissors(2).length).toStrictEqual(9);
+    expect(rockPaperScissors(5).length).toStrictEqual(243);
+  });
+});
